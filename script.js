63a041d0fe1c1ea3d740e010e090c7f4b71c3745
Gabriela L. Poças <106157345+Bibi-1234@users.noreply.github.com>
2023-11-16 12:01:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+window.sr = ScrollReveal({ reset: true});
+
+sr.reveal('.criadores', { duretion: -1000});
+sr.reveal('.projetos', { duretion: -1000});
+
+sr.reveal('.logo', { duretion: -1000});
+sr.reveal('.titulo-principal', { duretion: -1000});
+
+sr.reveal('.comeco', { duretion: -1000});
+sr.reveal('.texto', { duretion: -1000});
